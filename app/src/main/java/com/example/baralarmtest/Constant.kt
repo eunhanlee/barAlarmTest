e7fabe77a212b297de3781f892c9b5603709aef3
EH <eunhanlee58@gmail.com>
2022-02-16 15:48:05 -0500

    change to use companion object instead of string eesource

---
+++ app/src/main/java/com/example/baralarmtest/Constant.kt
@@ -0,0 +1,12 @@
+package com.example.baralarmtest
+
+class Constant {
+    companion object {
+        const val NOTIFICATION_ID = 0
+        const val NOTIFICATION_TITLE = "title"
+        const val NOTIFICATION_DESCRIPTION = "description"
+        const val CHANNEL_ID = "notification_channel"
+        const val CHANNEL_NAME = "notification_channel_name"
+        const val CHANNEL_DESCRIPTION = "notification_channel_description"
+    }
+}
--- app/src/main/java/com/example/baralarmtest/MainActivity.kt
+++ app/src/main/java/com/example/baralarmtest/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.baralarmtest
 
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import com.example.baralarmtest.Constant.Companion.CHANNEL_DESCRIPTION
+import com.example.baralarmtest.Constant.Companion.CHANNEL_ID
+import com.example.baralarmtest.Constant.Companion.CHANNEL_NAME
+import com.example.baralarmtest.Constant.Companion.NOTIFICATION_DESCRIPTION
+import com.example.baralarmtest.Constant.Companion.NOTIFICATION_ID
+import com.example.baralarmtest.Constant.Companion.NOTIFICATION_TITLE
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var notificationManager: NotificationManager
+    val context = this
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,34 +33,72 @@ class MainActivity : AppCompatActivity() {
 
         val button: Button = findViewById(R.id.button)//added
         button.setOnClickListener {
-            setBarAlarm(R.string.foodlight_start_alarm_message)
+            setNotification()
         }
     }
 
-    fun setBarAlarm(message: Int) {
-        val builder = NotificationCompat.Builder(this, getString(R.string.channel_id))
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentTitle(getString(R.string.alarm_title))
-            .setContentText(getString(message))
+    fun setNotification() {
+        notificationManager = context.getSystemService(
+            Context.NOTIFICATION_SERVICE
+        ) as NotificationManager
 
-        //after Oero 8.0 (API Level 26), channel creation is required
+        createNotificationChannel()
+        deliverNotification(context)
+
+    }
+
+    fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-            //NotificationChannel(channel_id, channel_name, 알림 우선순위)
-            val channel = NotificationChannel(
-                getString(R.string.channel_id),
-                getString(R.string.channel_name),
-                NotificationManager.IMPORTANCE_DEFAULT
-            ).apply {
-                description = getString(R.string.channel_description)
-            }
-
-            // set channel information to android system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-
-            notificationManager.notify(R.string.alarm_id, builder.build())
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID, // 채널의 아이디
+                CHANNEL_NAME, // 채널의 이름
+                NotificationManager.IMPORTANCE_HIGH
+                /*
+                    IMPORTANCE_HIGH - 알림음O 진동O 상태 표시줄O 내부 신호O 헤드업알림O
+                    IMPORTANCE_DEFAULT -알림음O 진동O 상태 표시줄O 내부 신호O
+                    IMPORTANCE_LOW - 알림음X 진동X 상태 표시줄O 내부 신호O
+                    IMPORTANCE_MIN - 알림음X 진동X 상태 표시줄X 내부 신호O
+                 */
+            )
+            notificationChannel.enableLights(true) // 불빛
+            notificationChannel.lightColor = Color.RED // 색상
+            notificationChannel.enableVibration(true) // 진동 여부
+            notificationChannel.description = CHANNEL_DESCRIPTION // 채널 정보
+            notificationManager.createNotificationChannel(
+                notificationChannel
+            )
         }
     }
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun deliverNotification(context: Context) {
+        val contentIntent = Intent(context, MainActivity::class.java)
+        val contentPendingIntent = PendingIntent
+            .getActivity(
+                context,
+                NOTIFICATION_ID, // requestCode
+                contentIntent, // 알림 클릭 시 이동할 인텐트
+                PendingIntent.FLAG_UPDATE_CURRENT
+                /*
+                1. FLAG_UPDATE_CURRENT : 현재 PendingIntent를 유지하고,
+                대신 인텐트의 extra data는 새로 전달된 Intent로 교체
+                2. FLAG_CANCEL_CURRENT : 현재 인텐트가 이미 등록되어있다면 삭제, 다시 등록
+                3. FLAG_NO_CREATE : 이미 등록된 인텐트가 있다면, null
+                4. FLAG_ONE_SHOT : 한번 사용되면, 그 다음에 다시 사용하지 않음
+                 */
+            )
+
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground) // 아이콘
+            .setContentTitle(NOTIFICATION_TITLE) // 제목
+            .setContentText(NOTIFICATION_DESCRIPTION) // 내용
+            .setContentIntent(contentPendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
+    }
+
+
 }
